b5718f58ef552b7da541aa603a1ddc20a931bde7
Cymer Denampo <cymerdenampo@gmail.com>
2025-04-22 11:34:27 +0800

    Add a login and Registration Page

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:forumapp/views/login_page.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'views/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Forum App',
+      home: LoginPage(),
+    );
+  }
+}
