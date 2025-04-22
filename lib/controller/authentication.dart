4aa94836ccfc3c28431c610ad9206db70b68d72d
Cymer Denampo <cymerdenampo@gmail.com>
2025-04-22 15:50:06 +0800

    add a snackbar error and redirection after successfull login and register

--- lib/controller/authentication.dart
+++ lib/controller/authentication.dart
@@ -1,11 +1,17 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:forumapp/constants/constants.dart';
+import 'package:forumapp/views/home.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 
 class AuthenticationController extends GetxController {
   final isLoading = false.obs;
+  final token = ''.obs;
+
+  final box = GetStorage();
 
   Future register({
     required String name,
@@ -23,7 +29,7 @@ class AuthenticationController extends GetxController {
       };
 
       var response = await http.post(
-        Uri.parse(url + 'register'),
+        Uri.parse('${url}register'),
         headers: {
           'Accept': 'application/json',
         },
@@ -32,11 +38,63 @@ class AuthenticationController extends GetxController {
 
       if (response.statusCode == 201) {
         isLoading.value = false;
-        // debugPrint(json.decode(response.body));
-        debugPrint(json.decode(response.body).toString());
+        // debugPrint(json.decode(response.body).toString());
+
+        token.value = json.decode(response.body)['token'];
+        box.write('token', token.value);
+        Get.offAll(() => const HomePage());
       } else {
         isLoading.value = false;
-        // debugPrint(json.decode(response.body));
+        Get.snackbar(
+          'Error',
+          json.decode(response.body)['message'],
+          snackPosition: SnackPosition.TOP,
+          backgroundColor: Colors.red,
+          colorText: Colors.white,
+        );
+        debugPrint(json.decode(response.body).toString());
+      }
+    } catch (e) {
+      isLoading.value = false;
+      print(e.toString());
+    }
+  }
+
+  Future login({
+    required String username,
+    required String password,
+  }) async {
+    isLoading.value = true;
+    try {
+      var data = {
+        'username': username,
+        'password': password,
+      };
+
+      var response = await http.post(
+        Uri.parse('${url}login'),
+        headers: {
+          'Accept': 'application/json',
+        },
+        body: data,
+      );
+
+      if (response.statusCode == 200) {
+        isLoading.value = false;
+        // debugPrint(json.decode(response.body).toString());
+
+        token.value = json.decode(response.body)['token'];
+        box.write('token', token.value);
+        Get.offAll(() => const HomePage());
+      } else {
+        isLoading.value = false;
+        Get.snackbar(
+          'Error',
+          json.decode(response.body)['message'],
+          snackPosition: SnackPosition.TOP,
+          backgroundColor: Colors.red,
+          colorText: Colors.white,
+        );
         debugPrint(json.decode(response.body).toString());
       }
     } catch (e) {
